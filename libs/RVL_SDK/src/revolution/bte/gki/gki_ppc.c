1267490e841b6fea35356a480b3432b323bf6c4d
Ahmed El Bannan <akelbann@gmail.com>
2026-08-01 19:07:10 +0800

    rvl: match GKI_send_event (us-802df260) via gki_ppc inlined disable/enable

--- libs/RVL_SDK/src/revolution/bte/gki/gki_ppc.c
+++ libs/RVL_SDK/src/revolution/bte/gki/gki_ppc.c
@@ -16,13 +16,31 @@ typedef unsigned int   UINT32;
 typedef struct {
     UINT8   OSIntNesting;                  /* 0x00 */
     UINT32  IntDisableCnt[26];             /* 0x04 */
+    UINT8   _pad[0x28802 - 0x6C];          /* up to OSInitFlag */
+    UINT8   OSInitFlag;                    /* 0x28802 */
+    UINT8   _pad2[0x28808 - 0x28803];      /* 0x28803..0x28807 */
+    UINT16  OSWaitEvt[8];                  /* 0x28808 */
 } tGKI_INT_MIRROR;
+
+#define GKI_MAX_TASKS 8
+#define GKI_SUCCESS   0x00
+#define GKI_FAILURE   0x01
 
 extern tGKI_INT_MIRROR gki_cb;
 
+void GKI_disable(void);
+void GKI_enable(void);
+
 void GKI_init() {}
 
-void GKI_shutdown() {}
+void GKI_shutdown(void) {
+    GKI_disable();
+    gki_cb.OSInitFlag = 0;
+    GKI_enable();
+    while (gki_cb.OSIntNesting != 0) {
+        GKI_enable();
+    }
+}
 
 void GKI_run(void) {
 }
@@ -36,7 +54,19 @@ void GKI_sched_unlock(void) {
 void GKI_delay(void) {
 }
 
-void GKI_send_event() {}
+UINT8 GKI_send_event(UINT8 task_id, UINT16 event) {
+    if (task_id >= GKI_MAX_TASKS) {
+        return GKI_FAILURE;
+    }
+
+    GKI_disable();
+
+    gki_cb.OSWaitEvt[task_id] |= event;
+
+    GKI_enable();
+
+    return GKI_SUCCESS;
+}
 
 int GKI_get_taskid(void) { return 0x2; }
 

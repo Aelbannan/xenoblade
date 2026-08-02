de101f2cb65cf8aa386684afa6c0dfd1d384ef07
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 03:48:56 +0800

    rvl: reconstruct __OSLaunchNextFirmware (firmware launch + disc boot path)

--- libs/RVL_SDK/src/revolution/os/OSExec.c
+++ libs/RVL_SDK/src/revolution/os/OSExec.c
@@ -314,6 +314,266 @@ int PackInstallerArgs(void* addr, s32 argc, char* argv[]) {
 }
 
 
+
+/* Externs for the firmware-launch / disc-boot path (dvd_broadway.h / OS.h). */
+typedef void (*DVDLowCallback)(u32 intType);
+extern BOOL DVDLowInit(void);
+extern BOOL DVDLowReadDiskID(DVDDiskID *out, DVDLowCallback callback);
+extern BOOL DVDLowClosePartition(DVDLowCallback callback);
+extern BOOL DVDLowUnencryptedRead(void *dst, u32 size, u32 offset,
+                                  DVDLowCallback callback);
+extern BOOL DVDLowFinalize(void);
+extern BOOL DVDLowOpenPartitionWithTmdAndTicketView(const u32 offset,
+                                                    const ESTicketView *ticket,
+                                                    const u32 ticketViewSize,
+                                                    const ESTitleMeta *tmd,
+                                                    const u32 tmdSize,
+                                                    const u8 *certs,
+                                                    DVDLowCallback callback);
+extern BOOL DVDLowOpenPartition(const u32 offset, const ESTicket *ticket,
+                                const u32 certsSize, const u8 *certs,
+                                ESTitleMeta *tmd, DVDLowCallback callback);
+extern u32 DVDLowIntType;
+extern void callback(u32 type);
+extern DVDDiskID id;
+
+extern void __OSGetPlayTime(ESTicketView *ticket, u32 *pLimit, u32 *pUsed);
+extern void __OSWriteExpiredFlag(void);
+extern BOOL OSPlayTimeIsLimited(void);
+extern void __OSReturnToMenuForError(void);
+extern void __OSHotResetForError(void);
+extern void __OSInitMemoryProtection(void);
+extern void __OSInitIPCBuffer(void);
+extern void IPCReInit(void);
+extern s32 IPCCltReInit(void);
+
+/* Static error/status strings (retail pool). */
+static const char sFailStr[] = "\nOSExec(): Failed to exec %d in %d\n";
+static const char sNoGameStr[] =
+    "\nOSExec(): The specified game doesn't exist in the disc\n";
+
+void __OSLaunchNextFirmware(void) {
+    u8 *bootInfo;
+    u8 *gameTmd;
+    ESTitleMeta *discTmd;
+    u8 *ticketView;
+    s32 err;
+    u32 count;
+    u32 tmdSize;
+    u32 limit;
+    u32 used;
+    u32 *p;
+    u8 *p2;
+    u32 i;
+    u64 titleId;
+    ESTicketView *views;
+    u32 codeStart;
+    u32 codeSize;
+
+    count = 1;
+    tmdSize = 0;
+    err = -1;
+
+    bootInfo = OSAllocFromMEM1ArenaLo(0x20, 0x20);
+    gameTmd = OSAllocFromMEM1ArenaLo(0x800, 0x20);
+    discTmd = OSAllocFromMEM1ArenaLo(0x4A00, 0x40);
+    ticketView = OSAllocFromMEM1ArenaLo(0xE0, 0x20);
+
+    if ((u32)__OSNextPartitionType == *(u32 *)0x80003194 &&
+        *(u32 *)0x80003198 != 0) {
+        err = ESP_InitLib();
+        if (err == 0) {
+            err = ESP_DiGetTicketView(NULL, (ESTicketView *)ticketView);
+        }
+        if (err == 0) {
+            err = ESP_DiGetTmd(NULL, &tmdSize);
+        }
+        if (err == 0) {
+            err = ESP_DiGetTmd(discTmd, &tmdSize);
+        }
+        ESP_CloseLib();
+
+        if (OSPlayTimeIsLimited()) {
+            limit = 0;
+            used = (u32)-1;
+            __OSGetPlayTime((ESTicketView *)ticketView, &limit, &used);
+            if (used == 0) {
+                __OSWriteExpiredFlag();
+                __OSReturnToMenuForError();
+            }
+        }
+    }
+
+    if (err == 0) {
+        p = (u32 *)gameTmd;
+        *(u32 *)(gameTmd + 4) = *(u32 *)0x80003194;
+        *(u32 *)(gameTmd + 0) = *(u32 *)0x80003198;
+    } else {
+        DVDLowIntType = 0;
+        DVDLowClosePartition(callback);
+        while (DVDLowIntType == 0) {
+            ;
+        }
+        if (DVDLowIntType != 1) {
+            OSReport(sFailStr, DVDLowIntType, 0x398);
+            __OSReturnToMenuForError();
+        }
+
+        DVDLowIntType = 0;
+        DVDLowUnencryptedRead(bootInfo, 0x20, 0x10000, callback);
+        while (DVDLowIntType == 0) {
+            ;
+        }
+        if (DVDLowIntType != 1) {
+            OSReport(sFailStr, DVDLowIntType, 0x3A2);
+            __OSReturnToMenuForError();
+        }
+
+        DVDLowIntType = 0;
+        DVDLowUnencryptedRead(gameTmd, 0x800, *(u32 *)(bootInfo + 4),
+                              callback);
+        while (DVDLowIntType == 0) {
+            ;
+        }
+        if (DVDLowIntType != 1) {
+            OSReport(sFailStr, DVDLowIntType, 0x3AE);
+            __OSReturnToMenuForError();
+        }
+
+        p = NULL;
+        p2 = gameTmd;
+        for (i = 0; i < *(u32 *)bootInfo; i++, p2 += 8) {
+            if (*(u32 *)(p2 + 4) == (u32)__OSNextPartitionType) {
+                p = (u32 *)p2;
+            }
+        }
+
+        if (p == NULL) {
+            p2 = gameTmd + 0x20;
+            for (i = 0; i < *(u32 *)(bootInfo + 8); i++, p2 += 8) {
+                if (*(u32 *)(p2 + 4) == (u32)__OSNextPartitionType) {
+                    p = (u32 *)p2;
+                }
+            }
+
+            if (p == NULL) {
+                OSReport(sNoGameStr);
+                __OSReturnToMenuForError();
+            }
+        }
+
+        *(u32 *)0x80003194 = *(u32 *)((u8 *)p + 4);
+        *(u32 *)0x80003198 = *(u32 *)((u8 *)p + 0);
+
+        DVDLowIntType = 0;
+        if (*(u8 *)0x80003187 == 0x80) {
+            DVDLowOpenPartitionWithTmdAndTicketView(
+                *(u32 *)((u8 *)p + 0), (ESTicketView *)ticketView, tmdSize,
+                discTmd, 0, NULL, callback);
+        } else {
+            DVDLowOpenPartition(*(u32 *)((u8 *)p + 0), NULL, 0, NULL,
+                                discTmd, callback);
+        }
+        while (DVDLowIntType == 0) {
+            ;
+        }
+        if (DVDLowIntType != 1) {
+            OSReport(sFailStr, DVDLowIntType, 0x3EB);
+            __OSReturnToMenuForError();
+        }
+
+        DVDLowIntType = 0;
+        DVDLowClosePartition(callback);
+        while (DVDLowIntType == 0) {
+            ;
+        }
+        if (DVDLowIntType != 1) {
+            OSReport(sFailStr, DVDLowIntType, 0x3F5);
+            __OSReturnToMenuForError();
+        }
+    }
+
+    titleId = *(u64 *)((u8 *)discTmd + 0x184);
+    err = ESP_InitLib();
+    if (err != 0) {
+        OSReport(sFailStr, err, 0x400);
+        __OSHotResetForError();
+    }
+
+    err = ESP_GetTicketViews(titleId, NULL, &count);
+    if (count != 1 || err != 0) {
+        OSReport(sFailStr, err, 0x409);
+        __OSHotResetForError();
+    }
+
+    views = OSAllocFromMEM1ArenaLo((count * 0xD8 + 0x1F) & ~0x1Fu, 0x20);
+    err = ESP_GetTicketViews(titleId, views, &count);
+    if (err != 0) {
+        OSReport(sFailStr, err, 0x412);
+        __OSHotResetForError();
+    }
+
+    DVDLowFinalize();
+
+    codeStart = *(u32 *)0x8000311C;
+    codeSize = *(u32 *)0x80003120;
+    DCStoreRange((void *)0x80003100, 0x100);
+
+    err = ESP_LaunchTitle(titleId, views);
+    if (err != 0) {
+        OSReport(sFailStr, err, 0x422);
+        __OSHotResetForError();
+    }
+
+    ESP_CloseLib();
+
+    DCInvalidateRange((void *)0x80003100, 0x100);
+
+    if (codeStart < *(u32 *)0x8000311C) {
+        *(u32 *)0x80003120 = *(u32 *)0x80003120 - (*(u32 *)0x8000311C - codeStart);
+        *(u32 *)0x80003128 = *(u32 *)0x80003128 - (*(u32 *)0x8000311C - codeStart);
+        *(u32 *)0x80003130 = *(u32 *)0x80003130 - (*(u32 *)0x8000311C - codeStart);
+        *(u32 *)0x80003134 = *(u32 *)0x80003134 - (*(u32 *)0x8000311C - codeStart);
+        *(u32 *)0x8000311C = codeStart;
+    }
+
+    if (codeSize < *(u32 *)0x80003120) {
+        __OSInitMemoryProtection();
+    }
+
+    __OSInitIPCBuffer();
+    IPCReInit();
+    IPCCltReInit();
+    DVDLowInit();
+
+    DVDLowIntType = 0;
+    DVDLowReadDiskID(&id, callback);
+    while (DVDLowIntType == 0) {
+        ;
+    }
+    if (DVDLowIntType != 1) {
+        OSReport(sFailStr, DVDLowIntType, 0x44E);
+        __OSReturnToMenuForError();
+    }
+
+    DVDLowIntType = 0;
+    if (*(u8 *)0x80003187 == 0x80) {
+        DVDLowOpenPartitionWithTmdAndTicketView(
+            *(u32 *)((u8 *)p + 0), (ESTicketView *)ticketView, tmdSize,
+            discTmd, 0, NULL, callback);
+    } else {
+        DVDLowOpenPartition(*(u32 *)((u8 *)p + 0), NULL, 0, NULL, discTmd,
+                            callback);
+    }
+    while (DVDLowIntType == 0) {
+        ;
+    }
+    if (DVDLowIntType != 1) {
+        OSReport(sFailStr, DVDLowIntType, 0x465);
+        __OSReturnToMenuForError();
+    }
+}
+
 void __OSBootDolSimple(s32 param1, u32 param2, u32 regionStart, u32 regionEnd,
                        s32 param5, u32 argc, void* argv);
 

3a05cd1c9f5c343879b53a995eb1974d779f1acd
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 01:23:18 +0800

    rvl: match lyt Bounding::DrawSelf (us-8032edd0) FULL_MATCH

--- libs/RVL_SDK/src/revolution/hbm/nw4hbm/lyt/lyt_bounding.cpp
+++ libs/RVL_SDK/src/revolution/hbm/nw4hbm/lyt/lyt_bounding.cpp
@@ -1,16 +1,6 @@
 #include <nw4hbm/lyt.h>
 
 namespace nw4hbm {
-namespace ut {
-
-/* Emitted here to match the retail binary: only lyt_bounding.o defines the
- * ut::Color destructor (other lyt TUs reference it via __destroy_arr). */
-Color::~Color() {}
-
-} // namespace ut
-} // namespace nw4hbm
-
-namespace nw4hbm {
 namespace lyt {
 
 NW4R_UT_RTTI_DEF_DERIVED(Bounding, Pane);
@@ -24,11 +14,23 @@ Bounding::Bounding(const res::Bounding* pRes, const ResBlockSet& rBlockSet)
 Bounding::~Bounding() {}
 
 void Bounding::DrawSelf(const DrawInfo& rInfo) {
-#pragma unused(rInfo)
-    // Debug draw stripped out
+    if (rInfo.IsDebugDrawMode()) {
+        LoadMtx(rInfo);
+
+        detail::DrawLine(GetVtxPos(), mSize, ut::Color::GREEN);
+    }
 }
 
 } // namespace lyt
 } // namespace nw4hbm
 
-extern "C" void* GetRuntimeTypeInfo__Q36nw4hbm3lyt8BoundingCFv(void) { return 0; }
+namespace nw4hbm {
+namespace ut {
+
+/* Emitted here to match the retail binary: only lyt_bounding.o defines the
+ * ut::Color destructor (other lyt TUs reference it via __destroy_arr). */
+Color::~Color() {}
+
+} // namespace ut
+} // namespace nw4hbm
+

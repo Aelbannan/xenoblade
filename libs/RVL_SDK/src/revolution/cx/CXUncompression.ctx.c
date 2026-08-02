1c733e5209635f5f49496ac723c0608647c61cb9
Ahmed El Bannan <akelbann@gmail.com>
2026-08-02 12:44:21 +0800

    rvl: match CXUncompression unit (us-8030b6c0 FULL_MATCH; us-8030b700 95.8%/0 structural, SMT stalled)

--- libs/RVL_SDK/src/revolution/cx/CXUncompression.ctx.c
+++ libs/RVL_SDK/src/revolution/cx/CXUncompression.ctx.c
@@ -168,19 +168,11 @@ namespace std{
     class exception{
     public:
         exception(){}
-        virtual ~exception(){}
-        virtual const char* what() const {
-            return "exception";
-        }
     };
 
     class bad_exception : public exception {
     public:
         bad_exception(){}
-        virtual ~bad_exception(){}
-        virtual const char* what() const {
-            return "bad_exception";
-        }
     };
 
     typedef void (*unexpected_handler)();
@@ -722,50 +714,79 @@ typedef int BOOL;
 #endif
 /* end "revolution/cx/CXUncompression.h" */
 
-//unused
-void CXGetUncompressedSize(){
+// Byte-swap a 32-bit word (the CX streams store integers big-endian).
+static inline u32 CXSwap32(u32 val) {
+    return (val >> 24 & 0xFF) | (val >> 8 & 0xFF00) | (val << 8 & 0xFF0000) |
+           (val << 24 & 0xFF000000);
 }
 
-//unused
-void CXUncompressAny(){
+// Load a big-endian 32-bit word and return its numeric value.
+static inline u32 CXLoadBE32(const void* p) {
+    return CXSwap32(*(const u32*)p);
 }
 
-//unused
-void CXUncompressRL(){
+u32 CXGetUncompressedSize(const u8* src) {
+    u32 size = CXLoadBE32(src) >> 8;
+    if (size == 0) {
+        size = CXLoadBE32(src + 4);
+    }
+    return size;
 }
 
-//unused
-void CXUncompressLZ(){
-}
+void CXUncompressHuffman(const u8* src, u8* dst) {
+    s32 size;
+    u32 shift;
+    u32 bitBuffer;
+    u32 byteCount;
+    u32 countMax;
+    const u8* data;
+    u32 treeBase;
+    const u8* tree;
+    const u8* words;
+    u32 bits;
+    s32 bitsLeft;
 
-//unused
-void CXUncompressHuffman(){
-}
+    size = CXLoadBE32(src) >> 8;
+    if (size != 0) {
+        data = src + 4;
+    } else {
+        data = src + 8;
+    }
 
-//unused
-void CXiHuffImportTree(){
-}
+    shift = src[0] & 0xF;
+    bitBuffer = 0;
+    treeBase = (u32)(data + 1);
+    countMax = (src[0] & 7) + 4;
+    byteCount = 0;
+    if (size == 0) {
+        size = CXLoadBE32(src + 4);
+    }
 
-//unused
-void CXUncompressLH(){
-}
+    tree = (const u8*)treeBase;
+    words = data + 2 * (data[0] + 1);
 
-//unused
-void RCAddCount_(){
-}
-
-//unused
-void RCSearch_(){
-}
-
-//unused
-void CXUncompressLRC(){
-}
-
-//unused
-void CXUnfilterDiff(){
-}
-
-//unused
-void CXGetCompressionHeader(){
+    while (size > 0) {
+        bits = CXLoadBE32(words);
+        words += 4;
+        bitsLeft = 32;
+        while (--bitsLeft >= 0) {
+            u8 node = *tree;
+            u32 bit = bits >> 31;
+            tree = (const u8*)(((u32)tree & ~1) + bit + 2 * ((node & 0x3F) + 1));
+            if ((node << bit) & 0x80) {
+                u8 out = *tree;
+                bitBuffer = (bitBuffer >> shift) | (out << (32 - shift));
+                tree = (const u8*)treeBase;
+                if (++byteCount == countMax) {
+                    *(u32*)dst = CXSwap32(bitBuffer);
+                    dst += 4;
+                    byteCount = 0;
+                    size -= 4;
+                }
+            }
+            if (size <= 0)
+                break;
+            bits <<= 1;
+        }
+    }
 }

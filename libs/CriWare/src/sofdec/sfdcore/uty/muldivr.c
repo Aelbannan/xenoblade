917401380ee04502b141b48a29694bdea018da70
Ahmed El Bannan <akelbann@gmail.com>
2026-08-04 03:17:47 +0800

    cri: record muldivr/cri_crw_std in-progress states + ItoA stall note

--- libs/CriWare/src/sofdec/sfdcore/uty/muldivr.c
+++ libs/CriWare/src/sofdec/sfdcore/uty/muldivr.c
@@ -3,11 +3,9 @@
 s64 UTY_MulDivRound64(s64 a, s64 b, s64 c) {
     s32 sign = 1;
     u64 n;
-    s32 cpos;
     if (c == 0) {
         return ((a ^ b) < 0) ? 0x8000000000000000LL : 0x7FFFFFFFFFFFFFFFLL;
     }
-    cpos = (c > 0);
     if (a < 0) {
         a = -a;
         sign = -sign;
@@ -21,10 +19,9 @@ s64 UTY_MulDivRound64(s64 a, s64 b, s64 c) {
         sign = -sign;
     }
     n = (u64)a * (u64)b;
-    n += (u64)(c >> 1) + cpos;
+    n += (u64)(c >> 1) + (c > 0);
     n = (s64)n / c;
-    if (sign < 0) {
-        n = -n;
-    }
+    if (sign < 0)
+        n = (u64)(-(s64)n);
     return (s64)n;
 }

56268ce17093afe13980f584e13acf9209b4e25f
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 13:08:00 +0800

    criware: implement AIFF_GetInfo 0%→3.8% + ADXB_CheckAiff FULL_MATCH (AIFF FORM/COMM/SSND chunk walker, LE32/swap32 helpers; alloc cascade walls)

--- libs/CriWare/src/adx/adxt/adx_baif.c
+++ libs/CriWare/src/adx/adxt/adx_baif.c
@@ -3,7 +3,59 @@
 
 #include <harness_catalog.h>
 
-void AIFF_GetInfo() {}
+static inline u32 AiffLE32(const u8* p) {
+    return (u32)p[0] | ((u32)p[1] << 8) | ((u32)p[2] << 16) | ((u32)p[3] << 24);
+}
+
+static inline u32 AiffSwap32(u32 x) {
+    return (((x >> 8) & 0xFF) << 16) | (((x >> 16) & 0xFF) << 8) | ((x & 0xFF) << 24) | ((x >> 24) & 0xFF);
+}
+
+static inline u32 AiffLE16(const u8* p) {
+    return (u32)p[0] | ((u32)p[1] << 8);
+}
+
+static inline u32 AiffSwap16(u32 x) {
+    return ((x >> 8) & 0xFF) | ((x & 0xFF) << 8);
+}
+
+int AIFF_GetInfo(u8* self, u32* out1, u32* out2, u32* out3, u32* out4) {
+    u8* p = self + 12;
+    u32 comm = 0;
+    if (AiffLE32(self) != 0x4D524F46) return 0;
+    if (AiffLE32(self + 8) != 0x46464941) return 0;
+    {
+        u8* end = p + AiffSwap32(AiffLE32(self + 4)) - 4;
+        u32 ssnd = 0;
+        while (p < end) {
+        u32 type = AiffLE32(p);
+        u32 size = AiffLE32(p + 4);
+        p += 8;
+        if (type == 0x4D4D4F43) {
+            if (comm == 0) {
+                if (AiffSwap32(size) <= 18) return 0;
+                *out2 = AiffSwap16(AiffLE16(p));
+                *out4 = AiffSwap32(AiffLE32(p + 2));
+                *out3 = AiffSwap16(AiffLE16(p + 6));
+                *out1 = AiffSwap32(AiffLE32(p + 8));
+                comm = 1;
+            }
+            if (ssnd != 0) goto out;
+        } else if (type == 0x444E5353) {
+            if (ssnd == 0) {
+                u32 off = AiffSwap32(AiffLE32(p));
+                ssnd = 1;
+                p += 4;
+                if (comm != 0) return (int)(p + off);
+            }
+        } else {
+            p += (AiffSwap32(size) + 1) & ~1;
+        }
+        }
+    }
+out:
+    ;
+}
 
 extern char lbl_eu_80516B20[];
 

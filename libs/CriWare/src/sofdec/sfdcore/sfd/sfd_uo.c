c0a3cc86b560f0bbdd08c5bb3c583c705ce4ea32
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 19:00:35 +0800

    criware: SFUO_Create semantic impl (loop structure needs iteration)

--- libs/CriWare/src/sofdec/sfdcore/sfd/sfd_uo.c
+++ libs/CriWare/src/sofdec/sfdcore/sfd/sfd_uo.c
@@ -11,7 +11,27 @@ int SFUO_Finish(void) { return 0x0; }
 
 void SFUO_ExecServer() {}
 
-void SFUO_Create() {}
+extern void SFBUF_SetUoch(void* entry, void* a, void* b, void* c);
+
+s32 SFUO_Create(void* self) {
+    u8* end = (u8*)self + 0x2638;
+    u8* e = (u8*)self + 4;
+    u32 i = 0;
+    u32 j = 0;
+    *(u8**)((u8*)self + 0x2200) = end;
+    *(u32*)((u8*)self + 0x2638) = 0;
+    *(u8**)((u8*)e + 0) = end;
+    do {
+        *(u8**)((u8*)e + 4) = end;
+        *(u8**)((u8*)e + 8) = end;
+        *(u8**)((u8*)e + 12) = end;
+        SFBUF_SetUoch(e, (void*)j, (void*)i, (void*)j);
+        i++;
+        e += 16;
+        j += 16;
+    } while (i < 3);
+    return 0;
+}
 
 int SFUO_Destroy(void) { return 0x0; }
 

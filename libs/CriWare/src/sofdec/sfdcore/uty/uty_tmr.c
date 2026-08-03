d5bfb76360b877aacd9d6195c9ab34aa3054fbb6
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 08:34:11 +0800

    criware: implement UTY_InitTmr 0%→54.3% (volatile tmr reload, CRICFG_Read default, 0x800000F8 >>2)

--- libs/CriWare/src/sofdec/sfdcore/uty/uty_tmr.c
+++ libs/CriWare/src/sofdec/sfdcore/uty/uty_tmr.c
@@ -3,7 +3,29 @@
 
 #include <harness_catalog.h>
 
-void UTY_InitTmr() {}
+extern char lbl_eu_8051CF20[];
+extern s32 lbl_eu_80619BF0;
+extern s32 CRICFG_Read(const char* key, s32* out);
+
+void UTY_InitTmr(void* self) {
+    s32 val;
+    volatile u32* tmr = (volatile u32*)&lbl_eu_80619BF0;
+    volatile u32* p2 = tmr + 2;
+    if (CRICFG_Read(lbl_eu_8051CF20, &val) == 0) {
+        self = (void*)val;
+    }
+    tmr[0]++;
+    if (!((s32)tmr[0] > 1 && (s32)tmr[1] == (s32)self)) {
+        tmr[1] = (u32)self;
+        if ((s32)self == -1) {
+            p2[1] = 1;
+            p2[0] = 0;
+        } else {
+            p2[1] = (*(volatile u32*)0x800000F8) >> 2;
+            p2[0] = 0;
+        }
+    }
+}
 
 extern s32 lbl_eu_80619BF0;
 

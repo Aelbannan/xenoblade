ac0bfccc7977aeb15f3110682bbc8bd507630199
Ahmed El Bannan <akelbann@gmail.com>
2026-08-03 20:46:55 +0800

    criware: SFUO_Create corrected (end+4 entry, zero-stores, 4-arg SetUoch); 12 structural

--- libs/CriWare/src/sofdec/sfdcore/sfd/sfd_uo.c
+++ libs/CriWare/src/sofdec/sfdcore/sfd/sfd_uo.c
@@ -15,20 +15,19 @@ extern void SFBUF_SetUoch(void* entry, void* a, void* b, void* c);
 
 s32 SFUO_Create(void* self) {
     u8* end = (u8*)self + 0x2638;
-    u8* e = (u8*)self + 4;
+    u8* e = end + 4;
     u32 i = 0;
-    u32 j = 0;
-    *(u8**)((u8*)self + 0x2200) = end;
+    void* x = *(void**)((u8*)self + 0x2208);
+    *(void**)((u8*)self + 0x2200) = end;
     *(u32*)((u8*)self + 0x2638) = 0;
-    *(u8**)((u8*)e + 0) = end;
     do {
-        *(u8**)((u8*)e + 4) = end;
-        *(u8**)((u8*)e + 8) = end;
-        *(u8**)((u8*)e + 12) = end;
-        SFBUF_SetUoch(e, (void*)j, (void*)i, (void*)j);
+        *(u32*)((u8*)e + 0) = 0;
+        *(u32*)((u8*)e + 4) = 0;
+        *(u32*)((u8*)e + 8) = 0;
+        *(u32*)((u8*)e + 12) = 0;
+        SFBUF_SetUoch(self, x, (void*)i, e);
         i++;
         e += 16;
-        j += 16;
     } while (i < 3);
     return 0;
 }
